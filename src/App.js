e38f4b52780fe9ef7200187b84f8be09b4a3ca30
srisundar2024 <157309037+srisundar2024@users.noreply.github.com>
2024-04-16 10:32:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './CommonComponents/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        
+      </header> */}
+
+      <Header></Header>
+
+    </div>
+  );
+}
+
+export default App;
